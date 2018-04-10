6415b67bf51fb9b943a43e80a8de0e93d4706b15
Amartya Biswas <31181068+amartyabiswas@users.noreply.github.com>
2018-04-11 03:26:19 +0530

    Update player.js

--- public/js/player.js
+++ public/js/player.js
@@ -1,9 +1,5 @@
 var currPlayer = 0;
 
-// 0 - YouTube
-// 1 - Daily Motion
-// 2 - Vimeo
-
 // Gets all the player data
 socket.on('getPlayerData', function(data) {
     var roomnum = data.room;
@@ -38,11 +34,7 @@ socket.on('getPlayerData', function(data) {
 // Create Youtube Player
 socket.on('createYoutube', function(data) {
     if (currPlayer != 0) {
-        // var playerIn = document.getElementById("playerArea")
-        // console.log(playerIn.innerHTML)
-        // playerIn.innerHTML = "<iframe id=\"player\"allowfullscreen=\"0\"width=\"640\" height=\"360\"src=\"https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1\"frameborder=\"0\"style=\"border: solid 4px #37474F\"></iframe>"
-        // onYouTubeIframeAPIReady()
-
+       
         var daily = document.getElementById('dailyArea');
         daily.style.display = 'none';
 
@@ -69,10 +61,7 @@ socket.on('createDaily', function(data) {
     console.log("i am in create daily")
     // player.destroy()
     if (currPlayer != 1) {
-        //     var playerIn = document.getElementById("playerArea")
-        //     console.log(playerIn.innerHTML)
-        //     playerIn.innerHTML = "<iframe id=\"player-daily\" frameborder=\"0\" width=\"640\" height=\"360\"src=\"//www.dailymotion.com/embed/video/x26m1j4\"allowfullscreen allow=\"autoplay\"></iframe>"
-
+        
         var you = document.getElementById('playerArea');
         you.style.display = 'none';
 
